f1ce341f46148ac54f3bad9211c2278f3386692a
sindler-ferenc <56120116+sindler-ferenc@users.noreply.github.com>
2023-06-04 13:57:42 +0200

    Fix

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
